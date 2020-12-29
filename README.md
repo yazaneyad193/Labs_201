fde76cf0964d88dd139bbc5b6c2025ebd55cd6f2
yazaneyad193 <yazaneyad193@gmail.com>
2020-12-29 15:44:30 +0200

    Add New scripts

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Labs_201
+# Labs_201_Lab02
+
+*First Lab02 I make Prompt Statment and using Js*
+
+1. If statment 
+2. Switch
+
+> Aouthor Yazan
+---
+
+# Labs_201_Lab03
+
+*Here in lab 3 we will use order List and unorder List*
+
+1. Box Model
+
+2. List
+
+3. Table
+
+4. Js Loops
+
+5. Array
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
-
+let CountNum = 0;
 var YourName = prompt("Whats your name please :)");
-
+CountNum++;
 document.getElementById("WelcomeMassage").innerHTML =  YourName;
 
 
@@ -12,6 +12,7 @@ var UserInput = prompt("Do you think my name is Yazan type Yes / No");
 if (UserInput.toLowerCase() ==='yes' || UserInput.toUpperCase() === 'YES') {
     alert("Sure My Name Is Yazan Welcome Bro :)")
     console.log('My Name Is Yazan');
+    CountNum ++;
 } else {
     alert("Sorry isnt Correct , My Name is Yazan :(");
     console.log('Sorry isnt Correct :(');
@@ -22,6 +23,7 @@ var UserAge = prompt("Do you think my Age is 27 type Yes / No");
 if (UserAge.toLowerCase() ==='yes' || UserAge.toUpperCase() === 'YES') {
     alert("Sure My Age Is 27 Years Old Welcome Bro :)")
     console.log('My Age Is 27 Years Old');
+    CountNum++;
 } else {
     alert("Sorry isnt Correct ,my Age Is 27 Years Old :(");
     console.log('Sorry isnt Correct my  Age Is 27 Years Old:(');
@@ -33,6 +35,7 @@ var UserJob = prompt("Do you think my Job is Software Developer type Yes / No");
 if (UserJob.toLowerCase() ==='yes'|| UserJob.toUpperCase() === 'YES') {
     alert("Sure My Job is Software Developer Welcome Bro :)")
     console.log('My Job is Software Developer');
+    CountNum++;
 } else {
     alert("Sorry isnt Correct ,My Job is Software Developer :(");
     console.log('Sorry isnt Correct my  Job is Software Developer :(');
@@ -44,10 +47,59 @@ var UserLocation = prompt("Do you think my Location is Irbid type Yes / No");
 if (UserLocation.toLowerCase() ==='yes' || UserLocation.toUpperCase() === 'YES') {
     alert("Sure My Location is Irbid Welcome Bro :)")
     console.log('Location is Irbid');
+    CountNum++;
 } else {
     alert("Sorry isnt Correct ,My Location is Irbid :(");
     console.log('Sorry isnt Correct My Location is Irbid :(');
 }
 
 
+
+
+//LAB 3 Code Here
+
+var myNumberGuss = parseInt(prompt("Please guessing a number from 1 to 10 you have Four opportunities", ""));
+for (let index = 1; index <= 4; index++) {
+  
+    if (myNumberGuss >= 1 && myNumberGuss <= 9) {
+       
+        alert("Too Low");
+        console.log("Too Low "+myNumberGuss);
+        
+    }else if (myNumberGuss ===10){
+    
+        alert("Too High");
+        console.log("Too High "+myNumberGuss);
+        CountNum++;
+    
+    }
+    myNumberGuss = parseInt(prompt("Please guessing a number from 1 to 10", ""));
+}
+alert("My guessing number is 10");
+
+
+
+var myFruit = ["Apples", "mangoes"];
+
+
+var myFavuoritFruit = prompt("Please Guess which my two favourite fruit  choose one please you have 6 attempts (Apples /oranges / mandarins/ mangoes /Banana/ kiwifruit)");
+
+for (let index = 0; index < 5; index++) {
+   
+    if(myFavuoritFruit ===myFruit[0] ||myFavuoritFruit ===myFruit[1]){
+
+        alert("You have nice luck" + myFavuoritFruit);
+        console.log(" nice luck "+myFavuoritFruit);
+        CountNum++;
+        break;
+    }
+    else{
+        alert("Try Again Please");
+        console.log(" bad luck "+myFavuoritFruit);
+    }
+    myFavuoritFruit = prompt("Please Guess which my two favourite fruit  choose one please you have 6 attempts (Apples /oranges / mandarins/ mangoes /Banana/ kiwifruit)");
+}
+
+alert("Your Score is "+ CountNum + " From 7 :)" );
 alert("Welcome Bro " + YourName);
+
